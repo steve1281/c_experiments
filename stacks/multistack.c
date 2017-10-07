5fa68d14e3b186d7766a972ad1485a445b9be834
Steve Falcigno <steve1281@hotmail.com>
2017-10-07 09:27:22 -0400

    added dump and fixed bug

--- stacks/multistack.c
+++ stacks/multistack.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
-
+#include<string.h>
 
 #define OVERFLOW -1
 #define UNDERFLOW -1
@@ -19,9 +19,9 @@ int *top;
 void init(int stacks, int stacksize)
 {
     
-    int STACKSIZE = stacksize;
-    int STACKS= stacks;
-    int HEAPSIZE = (STACKSIZE*STACKS);
+    STACKSIZE = stacksize;
+    STACKS= stacks;
+    HEAPSIZE = (STACKSIZE*STACKS);
 
     // allocate
     stack = (int *)malloc(sizeof(int) * HEAPSIZE);
@@ -75,11 +75,46 @@ int push(int n, int v)
     stack[top[n]] = v;
 }
 
+/* 
+    banner 
+*/
+void banner()
+{
+    printf("+------------------------------+\n");
+    printf("|                              |\n");
+    printf("| N stacks in a single space   |\n");
+    printf("|                              |\n");
+    printf("+------------------------------+\n");
+    printf("\n");
+}
+
+void dump(int n)
+{
+    //char s[4000];
+    char *s = (char *)malloc(sizeof(char) * STACKSIZE * 4 );
+
+    char temp[4];
+    s[0]=0;
+
+    for (int i=bottom[n]+1; i<=top[n]; i++)
+    {
+        sprintf(temp,"%d,", stack[i]);
+        strcat(s, temp);
+    }
+    s[strlen(s)-1]=0; // remove last comma
+    printf ("%s\n",s); 
+   
+    free(s);
+    
+}
 /*
    test 1
 */
 void test1() 
 {
+    printf("Test 1: push 2 numbers on each of three stacks\n");
+    printf("And then pop 3 off each (so trigger an UNDERFLOW\n");
+    
     push(0,5);
     push(1,6);
     push(2,7);
@@ -88,9 +123,17 @@ void test1()
     push(1,9);
     push(2,10);
 
+    printf("\nContent of stacks 0,1,2\n");
+    dump(0);
+    dump(1);
+    dump(2);
+    printf("\n");
+
+    printf("\nPop stacks, and one extra\n");
     printf("%d %d %d\n", pop(0), pop(1), pop(2));
     printf("%d %d %d\n", pop(0), pop(1), pop(2));
     printf("%d %d %d\n", pop(0), pop(1), pop(2));
+    printf("\nDone.\n");
 }
 
 
@@ -105,8 +148,9 @@ int main()
     // errors are 
     //    OVERFLOW - stack is full
     //    EMPTY - stack can't pop
-
+    banner();
     init(3, 100);
     test1();  
     cleanup();
+    getchar();
 }
